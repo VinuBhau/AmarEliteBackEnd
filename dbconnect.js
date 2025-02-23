d8cf3558c40773e7f32b3743e375f0a872ec96bb
VinuBhau <roddvinayak98@gmail.com>
2025-02-23 09:48:49 +0530

    Update dbconnect.js

--- dbconnect.js
+++ dbconnect.js
@@ -1,15 +1,15 @@
 const { MongoClient } = require('mongodb');
 
 const mongoURI = 'mongodb+srv://anirudhmore96:Fullstackanirudh96@apartmentcluster.sa5bh.mongodb.net/';
-const client = new MongoClient(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const client = new MongoClient(mongoURI); // ✅ Removed deprecated options
 
 client.connect()
-.then(() => {
-    console.log('Connected to MongoDB');
-})
-.catch((err) => {
-    console.error('Failed to connect to MongoDB:', err);
-});
+  .then(() => {
+    console.log('✅ Connected to MongoDB');
+  })
+  .catch((err) => {
+    console.error('❌ Failed to connect to MongoDB:', err);
+  });
 
-
-module.exports=client
+module.exports = client;
